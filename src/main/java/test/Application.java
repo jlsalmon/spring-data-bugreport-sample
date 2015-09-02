66d492639287250c9c95f73718a17c62f6b6d6cf
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-09-02 09:40:17 +0200

    Added sample data

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -2,6 +2,7 @@ package test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 /**
@@ -11,6 +12,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class);
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+
+    RequestRepository requestRepository = context.getBean(RequestRepository.class);
+    PointRepository pointRepository = context.getBean(PointRepository.class);
+
+    Request request = new Request();
+    request.setId("1");
+    request.setName("request 1");
+    requestRepository.save(request);
+
+    Point point = new Point();
+    point.setId("1");
+    point.setName("point 1");
+    pointRepository.save(point);
   }
 }
